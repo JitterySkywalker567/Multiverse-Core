ef184d1af08913596d4533ed0a34176bd42c7552
ConnorTCM <cjcbatman@yahoo.com>
2024-07-07 16:50:26 -0400

    Initial commit

---
+++ src/main/java/tcm/multiversecore/MultiverseCoreClient.java
@@ -0,0 +1,10 @@
+package tcm.multiversecore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MultiverseCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
