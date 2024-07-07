d996e145a46dfce8c33dd06a368545c0cd371ad4
ConnorTCM <cjcbatman@yahoo.com>
2024-07-07 18:43:41 -0400

    Changed generic "Hello Fabric world!"

--- src/main/java/tcm/multiversecore/MultiverseCore.java
+++ src/main/java/tcm/multiversecore/MultiverseCore.java
@@ -11,6 +11,6 @@ public class MultiverseCore implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Multiverse Core has loaded");
 	}
 }
